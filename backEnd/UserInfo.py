e3efebef4d7300d8db70592a2701ff87038b1ca3
Qiu_Zeyuan <Qiu_zeyuan@126.com>
2021-11-15 23:45:47 +0800

    ✨ feat(UserInfo.py): 增加getActionList函数

--- backEnd/UserInfo.py
+++ backEnd/UserInfo.py
@@ -1,37 +1,27 @@
 import requests
-from requests.exceptions import RequestsWarning
+import time
 
 
-def getStar(userID):
-    data = requests.get('https://api.github.com/users/' +
-                        userID+'/starred').json()
-    starred_repos = []
-    for repo in data:
-        starred_repos.append(repo['full_name'])
-    return starred_repos
+def getActionList(userID: str, earlyTime: float) -> list:
+    """获取用户最近操作&时间列表``[[事件类型，发生时间],...]``
+        :param userID:用户名
+        :param earlyTime:查找时间起点
+        :return:返回一个列表
+    """
+    data = requests.get("https://api.github.com/users/" +
+                        userID+"/events").json()
+    
+    actionlist = []
+    for action in data:
+        if earlyTime <= time.mktime(time.strptime(action["created_at"], '%Y-%m-%dT%H:%M:%SZ')):
 
-def getRepository(userID):
-    data = requests.get('https://api.github.com/users/' +
-                        userID+'/repos').json()
-    starred_repos = []
-    for repo in data:
-        starred_repos.append(repo['full_name'])
-    return starred_repos
+            actionmsg = [action["type"], action["created_at"]]
+            actionlist.append(actionmsg)
 
-def getActionList(userID, earlyTime):
-
-    return
+    return actionlist
 
 
-def getFollowing(userID):
-    data = requests.get('https://api.github.com/users/' +
-                        userID + '/followers')
-    for user in data:
-        
-
-    return
-
-
-if __name__ == '__main__':
-    print(getStar('QiuZeyuan'))
-    print(getRepository('QiuZeyuan'))
+if __name__ == "__main__":
+    t = time.mktime(time.strptime(
+        '2021-11-12T12:31:31Z', '%Y-%m-%dT%H:%M:%SZ'))
+    print(getActionList('QiuZeyuan', t))
