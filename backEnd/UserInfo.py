e7e585056c350061a19844246fae7c520370a01f
Qiu_Zeyuan <Qiu_zeyuan@126.com>
2021-11-16 17:12:42 +0800

    修改返回时间格式，增加methods.py用来存储本后端需要的方法函数

--- backEnd/UserInfo.py
+++ backEnd/UserInfo.py
@@ -1,27 +1,26 @@
+from click.globals import push_context
 import requests
-import time
+from methods import *
 
 
 def getActionList(userID: str, earlyTime: float) -> list:
-    """获取用户最近操作&时间列表``[[事件类型，发生时间],...]``
+    """获取用户最近操作&时间列表``[(事件类型，发生时间),...]``
         :param userID:用户名
         :param earlyTime:查找时间起点
         :return:返回一个列表
     """
     data = requests.get("https://api.github.com/users/" +
                         userID+"/events").json()
-    
+    earlyTime = stemp2str(earlyTime)
     actionlist = []
     for action in data:
-        if earlyTime <= time.mktime(time.strptime(action["created_at"], '%Y-%m-%dT%H:%M:%SZ')):
-
-            actionmsg = [action["type"], action["created_at"]]
+        if earlyTime <= action["created_at"]: 
+            actionmsg = (action["type"], utc2chn(action["created_at"]))
             actionlist.append(actionmsg)
 
     return actionlist
 
 
 if __name__ == "__main__":
-    t = time.mktime(time.strptime(
-        '2021-11-12T12:31:31Z', '%Y-%m-%dT%H:%M:%SZ'))
-    print(getActionList('QiuZeyuan', t))
+    print(getActionList('QiuZeyuan',datetime.timestamp(datetime.strptime("2021-11-12T12:15:28Z", '%Y-%m-%dT%H:%M:%SZ'))))
+    pass
