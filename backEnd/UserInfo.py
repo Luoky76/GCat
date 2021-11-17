004ea19950610fcb3efaa8fbb6c98cd3a7d02371
Qiu_Zeyuan <Qiu_zeyuan@126.com>
2021-11-18 02:27:09 +0800

    修复了utc2cst函数bug

--- backEnd/UserInfo.py
+++ backEnd/UserInfo.py
@@ -1,18 +1,36 @@
 import requests
+import time
 from methods import *
 
-ACTIONTYPE = ("push", "commit", "pull", "remote", "merge")
+ActionTypes = ("push", "commit", "pull", "remote", "merge")
+UserReposApi = "https://api.github.com/users/{}/repos"
+UserEventsApi = "https://api.github.com/users/{}/events"
 
-def getActionList(userID: str, earlyTime: float, actionKind:str = ACTIONTYPE) -> tuple:
+
+def getNewRepositoryCnt(userID: str, earlyTime: float) -> int:
     """
+        获取 用户userID 在 指定时间earlyTime 后创建的仓库数量
+        :param userID:用户名
+        :param earlyTime:查找时间起点
+        :return:仓库数量
+    """
+    count = 0
+    data = requests.get(UserReposApi.format(userID)).json()  # 获取json数据
+    earlyTime = stemp2str(earlyTime)  # 时间格式化
+    reposList = [repo for repo in data if earlyTime <= repo["created_at"]]
+
+    return len(reposList)
+
+
+def getActionList(userID: str, earlyTime: float, actionKind: str = ActionTypes) -> list:
+    """ getActionList(userID,earlyTime[,actionKind])\n
         获取用户最近操作&时间二维表``[[事件类型，发生时间],...]``
         :param userID:用户名
         :param earlyTime:查找时间起点
         :param actionKind:指定类型
         :return:返回一个列表
     """
-    data = requests.get("https://api.github.com/users/" +
-                        userID+"/events").json()  # 获取json数据
+    data = requests.get(UserEventsApi.format(userID)).json()  # 获取json数据
     earlyTime = stemp2str(earlyTime)  # 时间格式化
     actionList = []  # 创建actionList空列表
 
@@ -23,5 +41,3 @@ def getActionList(userID: str, earlyTime: float, actionKind:str = ACTIONTYPE) ->
                 actionList.append(Action)
 
     return actionList
-
-
--- backEnd/methods.py
+++ backEnd/methods.py
@@ -1,26 +1,21 @@
 
 from datetime import datetime, timedelta
-from typing import Tuple
 
 
 def stemp2str(timeStamp: float):
     """
         将时间戳转为UTC时间字符串
     """
-    return datetime.strftime(datetime.utcfromtimestamp(timeStamp), '%Y-%m-%dT%H:%M:%SZ', )
+    return datetime.strftime(datetime.utcfromtimestamp(timeStamp), '%Y-%m-%dT%H:%M:%SZ' )
 
 
-def utc2chn(UTCstr: str):
+def utc2cst(UTCstr: str) -> str:
     """
-        将UTC时间字符串转化为北京时间字符串
+        将UTC时间字符串转化为CST时间字符串
     """
-
-    # return datetime.strftime(datetime.utcfromtimestamp(datetime.timestamp(datetime.strptime(UTCstr, '%Y-%m-%dT%H:%M:%SZ')))+timedelta(hours=16),'%Y/%m/%d %H:%M:%S')
-    stamp = datetime.timestamp(
-        datetime.strptime(UTCstr, '%Y-%m-%dT%H:%M:%SZ'))
-    UTCtime = datetime.utcfromtimestamp(stamp)
-    CHNtime = UTCtime+timedelta(hours=16)
-    CHNstr = CHNtime.strftime('%Y/%m/%d %H:%M')
+    UTCtime = datetime.strptime(UTCstr, '%Y-%m-%dT%H:%M:%SZ')
+    CHNtime = UTCtime+timedelta(hours=8)
+    CHNstr = CHNtime.strftime('%Y/%m/%d %H:%M:%S')
     return CHNstr
 
 
@@ -33,12 +28,12 @@ def ActionExtract(action: dict) -> tuple:
 
     if action["type"] == "PushEvent":
         ActionType = "push"
-        ActionTime = utc2chn(action["created_at"])
+        ActionTime = utc2cst(action["created_at"])
 
     elif action["type"] == "PullRequestEvent":
         if (action["payload"]["pull_request"]["merged_at"] != None):
             ActionType = "merge"
-            ActionTime = utc2chn(
+            ActionTime = utc2cst(
                 action["payload"]["pull_request"]["merged_at"])
 
     if ActionType and ActionTime:
