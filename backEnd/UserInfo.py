8cde2dbd22b5e0e4080750711eb2d3a588d0133b
高旭 <87891690+ShakingSH@users.noreply.github.com>
2021-11-18 20:50:20 +0800

    repositoryrcmd类编写完毕，已经经过单元测试

--- backEnd/UserInfo.py
+++ backEnd/UserInfo.py
@@ -1,26 +1,43 @@
-from click.globals import push_context
 import requests
+import time
 from methods import *
 
+ActionTypes = ("push", "commit", "pull", "remote", "merge")
+UserReposApi = "https://api.github.com/users/{}/repos"
+UserEventsApi = "https://api.github.com/users/{}/events"
 
-def getActionList(userID: str, earlyTime: float) -> list:
-    """获取用户最近操作&时间列表``[(事件类型，发生时间),...]``
+
+def getNewRepositoryCnt(userID: str, earlyTime: float) -> int:
+    """
+        获取 用户userID 在 指定时间earlyTime 后创建的仓库数量
         :param userID:用户名
         :param earlyTime:查找时间起点
+        :return:仓库数量
+    """
+    count = 0
+    data = requests.get(UserReposApi.format(userID)).json()  # 获取json数据
+    earlyTime = stemp2str(earlyTime)  # 时间格式化
+    reposList = [repo for repo in data if earlyTime <= repo["created_at"]]
+
+    return len(reposList)
+
+
+def getActionList(userID: str, earlyTime: float, actionKind: str = ActionTypes) -> list:
+    """ getActionList(userID,earlyTime[,actionKind])\n
+        获取用户最近操作&时间二维表``[[事件类型，发生时间],...]``
+        :param userID:用户名
+        :param earlyTime:查找时间起点
+        :param actionKind:指定类型
         :return:返回一个列表
     """
-    data = requests.get("https://api.github.com/users/" +
-                        userID+"/events").json()
-    earlyTime = stemp2str(earlyTime)
-    actionlist = []
+    data = requests.get(UserEventsApi.format(userID)).json()  # 获取json数据
+    earlyTime = stemp2str(earlyTime)  # 时间格式化
+    actionList = []  # 创建actionList空列表
+
     for action in data:
-        if earlyTime <= action["created_at"]: 
-            actionmsg = (action["type"], utc2chn(action["created_at"]))
-            actionlist.append(actionmsg)
+        if earlyTime <= action["created_at"]:  # earlyTime之后发生
+            Action = ActionExtract(action)
+            if Action and Action[0] in actionKind:
+                actionList.append(Action)
 
-    return actionlist
-
-
-if __name__ == "__main__":
-    print(getActionList('QiuZeyuan',datetime.timestamp(datetime.strptime("2021-11-12T12:15:28Z", '%Y-%m-%dT%H:%M:%SZ'))))
-    pass
+    return actionList
--- backEnd/app.py
+++ backEnd/app.py
@@ -1,15 +1,19 @@
-from flask import Flask
-from exts import db
-from models import Record
-import config
+from flask import Flask, request, json
+from GEvent import GEvent
+import handler
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/GcatServer', methods=['POST'])
+def main():
+    json_data = dict(request.get_json())
+    gEvent = GEvent(json_data)
+    print(11111111111111)
+    gEvent = handler.EventDistributer(gEvent)
+    
+    return gEvent
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
