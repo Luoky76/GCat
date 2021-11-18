129588208543ff40f951d907d6fa4f9175561e31
yuniwuyuan <email@671628094@qq.com>
2021-11-18 20:28:50 +0800

    ym

--- xcx.ym/pages/setup/setup.js
+++ xcx.ym/pages/setup/setup.js
@@ -1,66 +1,48 @@
-// pages/setup/setup.js
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+  data: {
+  show:false,//控制下拉列表的显示隐藏，false隐藏、true显示
+  List0:['user1', 'user2', 'user3', 'user4'],
+  List1:['Public', 'Private'],
+  index1: 0,
+  index2: 0,
+  },
+  // 点击下拉显示框
+  selectTap(){
+  this.setData({
+   show: !this.data.show
+  });
+  },
+  // 点击下拉列表
+  optionTap(e){
+  let Index1=e.currentTarget.dataset.index1;//获取点击的下拉列表的下标
+  let Index2=e.currentTarget.dataset.index2;//获取点击的下拉列表的下标
+  this.setData({
+   index1:Index1,
+   index2:Index2,
+   show:!this.data.show
+  });
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  bindButtonTap:function(){
+    this.setData({
+      focus:true
+    })
+  },
+  bindKeyInput:function(e){
+    this.setData({
+      inputValue:e.detail.value
+    })
+  },
+  bindHideKeyboard:function(e){
+    if(e.detail.value === "close"){
+      //收起键盘
+      wx.hideKeyboard();
     }
-})
+  }
+ })
