75299b80a1abf11672098eb3e03b1f99a81f88f2
fireflylxx <2965150024@qq.com>
2021-11-20 15:42:02 +0800

    add GitHubOperator

---
+++ GitHubOperator.py
@@ -0,0 +1,93 @@
+from github import Github
+import github.NamedUser
+import requests
+import time
+from github.Repository import Repository
+
+g = Github('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')
+# username = 'fireflylxx'
+
+user = g.get_user()
+# Actions = ('follow', 'declineFollow', 'star', 'declineStar')
+# creat_type = ('CreatRepo', 'DropRepo')
+#
+# repo = user.get_repos()
+# following = user.get_following()
+# star = user.get_starred()
+# print(type(g.get_repo("Luoky76/GCat")))
+# print(type(user))
+# user.add_to_starred(g.get_repo("Luoky76/GCat"))
+# user.create_repo("new-test")
+# user.add_to_starred(g.get_repo("Luoky76/GCat"))
+# FollowReposApi = 'https:api.github.com/users/{}/followers'
+# StarReposApi = 'https:api.github.com/users/{}/starred'
+# ReposApi = 'https:api.github.com/users/{}/repos'
+#
+# data = {
+#     "eventID": 422743327,
+#     "userID": "fireflylxx",
+#     "userToken": "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",
+#     "eType": "ChangeUserInfo",
+#     "eDetail": {
+#         "follow": "fireflylxx",
+#         "declineFollow": "ShakingSH",
+#         "star": "Luoky76/Gcat",
+#         "declineStar": "Luoky76/GCatDoc"
+#     },
+#     "eTime": 125599946663.62
+# }
+
+
+def follower(otheruserID:str) -> bool:
+    """获取用户关注"""
+    if user.has_in_following(otheruserID):
+        return False
+    else:
+        user.add_to_following()
+        return True
+
+
+def declineFollower(otheruserID:str) -> bool:
+    """取消用户关注"""
+    if user.has_in_following(otheruserID):
+        user.remove_from_following(otheruserID)
+        return True
+    else:
+        return False
+
+
+def star(repoID:str) -> bool:
+    """收藏"""
+    if user.has_in_starred(repoID):
+        return False
+    else:
+        user.add_to_starred(repoID)
+        return True
+
+
+def declineStar(repoID:str) -> bool:
+    """取关"""
+    if user.has_in_starred(repoID):
+        user.remove_from_starred(repoID)
+        return True
+    else:
+        return False
+
+
+def create_repo(repoID) -> str:
+    """创建仓库"""
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
